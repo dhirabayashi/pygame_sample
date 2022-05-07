81b559d372f2e960b4ae9e18729ad4ccae98fd16
daiki <gigabowser@hotmail.co.jp>
2022-05-07 23:56:37 +0900

    Initial commit

---
+++ draw_image_subregion1.py
@@ -0,0 +1,30 @@
+import sys
+import pygame
+from pygame.locals import QUIT, Rect
+
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 200))
+FPSLOCK = pygame.time.Clock()
+
+
+def main():
+    """main routine"""
+    logo = pygame.image.load('pythonlogo.jpg')
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        SURFACE.fill((225, 225, 225))
+        SURFACE.blit(logo, (0, 0))
+        SURFACE.blit(logo, (250, 50), Rect(50, 50, 100, 100))
+
+        pygame.display.update()
+        FPSLOCK.tick(3)
+
+
+if __name__ == '__main__':
+    main()
