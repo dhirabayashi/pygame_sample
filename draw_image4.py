95376812d3d6fada04d7f867dd24385225d9586e
daiki <gigabowser@hotmail.co.jp>
2022-05-08 16:59:26 +0900

    画像を回転するサンプルの追加

---
+++ draw_image4.py
@@ -0,0 +1,36 @@
+import sys
+import pygame
+from pygame.locals import QUIT
+
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 300))
+FPSCLOCK = pygame.time.Clock()
+
+
+def main():
+    """main routine"""
+
+    logo = pygame.image.load('pythonlogo.jpg')
+    theta = 0
+
+    while True:
+        for event in pygame.event.get(QUIT):
+            pygame.quit()
+            sys.exit()
+
+        theta += 1
+        SURFACE.fill((225, 225, 225))
+
+        # ロゴを回転し、中心が(200, 150)の位置にロゴを描画
+        new_logo = pygame.transform.rotate(logo, theta)
+        rect = new_logo.get_rect()
+        rect.center = (200, 150)
+        SURFACE.blit(new_logo, rect)
+
+        pygame.display.update()
+        FPSCLOCK.tick(3)
+
+
+if __name__ == '__main__':
+    main()
